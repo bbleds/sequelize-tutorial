bcf4f866e475bddf7e168dea22abde2e638b2282
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-01 10:31:08 -0600

    following scott

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 
+const Genre = require('./models/').Genre;
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -9,6 +11,12 @@ const app = express();
 app.get('/', (req, res) => {
   res.send({
     status: 'Success'
+  });
+});
+
+app.get('/genres', (req, res) => {
+  Genre.findAll().then((genres) => {
+    res.send(genres);
   });
 });
 
